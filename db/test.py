c09b9412f57ad11f68644be1ee3e87e0be8026e7
JulianChu-1 <1664553649@qq.com>
2024-04-06 15:07:10 +0800

    new file:   1/11.py 	new file:   1/aa.ipynb 	new file:   1/data_topic.xlsx 	new file:   1/lda_pass8.html 	new file:   1/learn.ipynb 	new file:   data/data.xlsx 	new file:   data/data_test.xlsx 	new file:   data/my_dict.txt 	new file:   data/pre/edu.csv 	new file:   data/pre/entertain.csv 	new file:   data/pre/finance.csv 	new file:   data/pre/international.csv 	new file:   data/pre/military.csv 	new file:   data/pre/sport.csv 	new file:   data/pre/tech.csv 	new file:   data/test_dict.txt 	new file:   data/test_stopwords.txt 	new file:   data/toutiao_cat_data.txt 	new file:   db/test.py

---
+++ db/test.py
@@ -0,0 +1,24 @@
+import pymongo
+
+client = pymongo.MongoClient(host='127.0.0.1:27017')
+my_collection = client['test']['test01']
+
+item_1 = {
+  "_id" : "U1IT00001",
+  "item_name" : "Blender",
+  "max_discount" : "10%",
+  "batch_number" : "RR450020FRG",
+  "price" : 340,
+  "category" : "kitchen appliance"
+}
+
+item_2 = {
+  "_id" : "U1IT00002",
+  "item_name" : "Egg",
+  "category" : "food",
+  "quantity" : 12,
+  "price" : 36,
+  "item_description" : "brown country eggs"
+}
+
+my_collection.insert_many([item_1,item_2])
